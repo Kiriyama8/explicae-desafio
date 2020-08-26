4b4c0f0554b7253d76eff79f1469870f5228ac7a
Kiriyama8 <minumex8@gmail.com>
2020-08-26 12:35:24 -0300

    Adiciona a documentação dos métodos da classe

--- pages/questao_03/index.php
+++ pages/questao_03/index.php
@@ -1,16 +1,39 @@
 <?php
 
-class FileReader {
+/**
+ * Class FileReader
+ * @version 1.0.0
+ */
+class FileReader
+{
+	/** @var string $sConteudo */
 	private $sConteudo;
 
+	/**
+	 * Função responsável por abrir e fazer a leitura do arquivo informado
+	 *
+	 * @param string $sCaminho
+	 * @return void
+	 *
+	 * @author Vinícius Barros minumex8@gmail.com
+	 */
 	public function carregar(string $sCaminho): void {
 		$sArquivo = fopen($sCaminho, "r");
+
 		while(!feof($sArquivo)) {
 			$this->sConteudo .= fread($sArquivo, filesize($sCaminho));
 		}
+
 		fclose($sArquivo);
 	}
 
+	/**
+	 * Função responsável por exibir o conteúdo do arquivo lido
+	 *
+	 * @return void
+	 *
+	 * @author Vinícius Barros minumex8@gmail.com
+	 */
 	public function visualizar(): void {
 		echo $this->sConteudo;
 	}
