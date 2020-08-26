80b5b0cfa3990f7fe7149efdacd0cdd2a2957120
Kiriyama8 <minumex8@gmail.com>
2020-08-26 12:29:28 -0300

    Corrige descrição, nome e parâmetros das funções, atribui a uma variável o retorno de uma função.

--- pages/questao_01/index.php
+++ pages/questao_01/index.php
@@ -1,52 +1,56 @@
 <?php require_once '../../_partials/header.php'; ?>
 <?php
-	/**
-	 * Função responsável por gerar um intervalo de números por default entre 1 e 60.
-	 *
-	 * @param int $iMin
-	 * @param int $iMax
-	 * @return array
-	 *
-	 * @author Vinícius Barros minumex8@gmail.com
-	 */
-	function gerarNumerosMegasena(int $iMin = 1, int $iMax = 60): array {
-		return range($iMin, $iMax);
+/**
+ * Função responsável por gerar um intervalo de números por default entre 1 e 60.
+ *
+ * @param int $iMin
+ * @param int $iMax
+ * @return array
+ *
+ * @author Vinícius Barros minumex8@gmail.com
+ */
+function gerarIntervaloDeNumeros(int $iMin = 1, int $iMax = 60): array {
+	return range($iMin, $iMax);
+}
+
+/**
+ * Função responsável por gerar 6 números premiados de acordo com o intervalo
+ * de número informado, por default entre 1 e 60.
+ *
+ * @param int $iMin
+ * @param int $iMax
+ * @param int $iQuantidade
+ * @return array
+ *
+ * @author Vinícius Barros minumex8@gmail.com
+ */
+function gerarNumerosPremiados(int $iMin = 1, int $iMax = 60, int $iQuantidade = 6): array {
+	$aNumerosGerado = [];
+
+	while (count($aNumerosGerado) < $iQuantidade) {
+		$iNumeroGerado = mt_rand($iMin, $iMax);
+		$aNumerosGerado[$iNumeroGerado] = $iNumeroGerado;
 	}
 
-	/**
-	 * Função responsável por gerar 6 números premiados de acordo com o intervalo
-	 * de número informado, por default entre 1 e 60.
-	 *
-	 * @param int $iMin
-	 * @param int $iMax
-	 * @param int $iQuantity
-	 * @return array
-	 *
-	 * @author Vinícius Barros minumex8@gmail.com
-	 */
-	function gerarNumerosPremiados(int $iMin = 1, int $iMax = 60, int $iQuantity = 6): array {
-		$aNumerosGerado = [];
+	sort($aNumerosGerado);
 
-		while (count($aNumerosGerado) < $iQuantity) {
-			$iNumeroGerado = mt_rand($iMin, $iMax);
-			$aNumerosGerado[$iNumeroGerado] = $iNumeroGerado;
-		}
+	return $aNumerosGerado;
+}
 
-		sort($aNumerosGerado);
+/**
+ * Função responsável por transformar um array em uma string
+ *
+ * @param array $aArray
+ * @return string
+ *
+ * @author Vinícius Barros minumex8@gmail.com
+ */
+function formatarArrayParaString(array $aArray): string {
+	return implode(' - ', $aArray);
+}
 
-		return $aNumerosGerado;
-	}
-
-	/**
-	 * Função responsável por transformar o array com os números premiados em uma string
-	 *
-	 * @return string
-	 *
-	 * @author Vinícius Barros minumex8@gmail.com
-	 */
-	function exibirNumerosPremiados(): string {
-		return implode(' - ', gerarNumerosPremiados());
-	}
+$aNumerosSorteados = gerarNumerosPremiados();
+$aNumeros = gerarIntervaloDeNumeros();
 ?>
 
 <div class="container pt-5">
@@ -54,7 +58,7 @@
 		<div class="col-8 offset-2">
 			<table class="table">
 				<caption class="bg-primary text-white font-weight-bold pl-3">
-					Números Sorteados: <?= exibirNumerosPremiados(); ?>
+					Números Sorteados: <?= formatarArrayParaString($aNumerosSorteados); ?>
 				</caption>
 				<thead>
 				<tr>
@@ -63,7 +67,7 @@
 				</thead>
 				<tbody>
 				<?php
-					foreach (gerarNumerosMegasena() as $iNumero) {
+					foreach ($aNumeros as $iNumero) {
 						if ($iNumero % 10 == 1) {
 							echo "<tr>";
 						}
